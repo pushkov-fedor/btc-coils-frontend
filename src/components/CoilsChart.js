c9cd232b5b4aad8cf64358af19b0cfbdf0ae8959
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-09-09 02:09:29 +0700

    add scalable xAxis

--- src/components/CoilsChart.js
+++ src/components/CoilsChart.js
@@ -1,68 +1,94 @@
 import React, { useEffect } from "react";
 import * as d3 from "d3";
-import generateBtcPrice from "../util/generateBtcPrice";
+import generateBtcPrice, { generateSimplyXY } from "../util/generateBtcPrice";
 const btcPrice = 11434.1;
 const generator = generateBtcPrice(btcPrice);
 
+const generateBarChartData = (maxValue) => {
+  const data = [];
+  let charCode = "A".charCodeAt(0);
+  for (let i = 0; i < 26; i++) {
+    data.push({
+      name: String.fromCharCode(charCode),
+      value: Math.random() * maxValue,
+    });
+    charCode++;
+  }
+  return data;
+};
+
 export default function CoilsChart() {
   useEffect(() => {
-    const data = [];
-    const width = 500;
+    const margin = { top: 20, right: 0, bottom: 30, left: 40 };
     const height = 500;
-    let globalX = 0;
-    const duration = 500;
-    const max = 500;
-    const step = 1;
-    const chart = d3
+    const width = 500;
+    const data = generateBarChartData(120);
+
+    const svg = d3
       .select("#coils-chart")
       .append("svg")
-      .attr("width", width + 50)
-      .attr("height", height + 50)
-      .style("background", "#f4f4f4")
-      .call(
-        d3.zoom().on("zoom", function () {
-          chart.attr("transform", d3.event.transform);
-        })
-      )
-      .append("g");
-    const x = d3.scaleLinear().domain([0, 5000]).range([0, 500]);
+      .attr("viewBox", [0, 0, width, height]);
+
+    const x = d3
+      .scaleBand()
+      .domain(data.map((d) => d.name))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
     const y = d3
       .scaleLinear()
-      .domain([btcPrice - 500, btcPrice + 500])
-      .range([500, 0]);
+      .domain([0, d3.max(data, (d) => d.value)])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
 
-    const line = d3
-      .line()
-      .x((d) => x(d.time))
-      .y((d) => y(d.price));
-
-    const xAxis = d3.axisBottom().scale(x);
-    const axisX = chart
+    const xAxis = svg
       .append("g")
-      .attr("transform", "translate(25,500)")
-      .call(xAxis);
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+    const yAxis = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y))
+      .call((g) => g.select(".domain").remove());
 
-    const path = chart.append("path");
-    const tick = () => {
-      const point = generator.next().value;
-      data.push(point);
-      globalX += step;
-      path
-        .datum(data)
-        .attr("d", line)
-        .style("fill", "none")
-        .style("stroke", "green");
-      x.domain([globalX - (max - step), globalX]);
-      axisX.transition().duration(duration).ease(d3.easeLinear, 2).call(xAxis);
+    svg
+      .append("g")
+      .attr("class", "bars")
+      .attr("fill", "green")
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", (d) => x(d.name))
+      .attr("y", (d) => y(d.value))
+      .attr("height", (d) => y(0) - y(d.value))
+      .attr("width", x.bandwidth());
 
-      path
-        .attr("transform", null)
-        .transition()
-        .duration(duration)
-        .ease(d3.easeLinear, 2)
-        .attr("transform", "translate(" + x(globalX - max) + ")");
-    };
-    setInterval(() => tick(), 100);
+    const extent = [
+      [margin.left, margin.top],
+      [width - margin.right, height - margin.top],
+    ];
+
+    svg.call(
+      d3
+        .zoom()
+        .scaleExtent([1, 8])
+        .translateExtent(extent)
+        .extent(extent)
+        .on("zoom", zoomed)
+    );
+    function zoomed() {
+      const event = d3.event;
+      x.range(
+        [margin.left, width - margin.right].map((d) =>
+          event.transform.applyX(d)
+        )
+      );
+      svg
+        .selectAll(".bars rect")
+        .attr("x", (d) => x(d.name))
+        .attr("width", x.bandwidth());
+      svg.selectAll(".x-axis").call(d3.axisBottom(x).tickSizeOuter(0));
+    }
   }, []);
   return <div id="coils-chart" style={{ height: "100vh" }}></div>;
 }
